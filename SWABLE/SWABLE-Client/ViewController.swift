da1b62283052cea7fa0b6a04203ac72e96ffd165
Rob Visentin <jvisenti@gmail.com>
2018-01-16 15:51:41 -0500

    added stub projects for client and server test apps

---
+++ SWABLE/SWABLE-Client/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SWABLE
+//
+//  Created by Rob Visentin on 1/16/18.
+//  Copyright © 2018 Raizlabs. All rights reserved.
+//
+
+import Anchorage
+
+class ViewController: UIViewController {
+
+    override func loadView() {
+        view = UIView()
+        view.backgroundColor = .white
+    }
+
+}
+
