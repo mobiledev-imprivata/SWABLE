7154122525524b136411e2217ce0618bc6443b28
Jonathan Cole <joncole04412@gmail.com>
2018-01-18 13:47:21 -0500

    Big update to Parasite. Should no longer crash when running out of RAM.

---
+++ SWABLE/Parasite/Dependencies/RZIOKit/RZIOKit.c
@@ -0,0 +1,135 @@
+//
+//  RZIOKit.c
+//
+//  Created by Rob Visentin on 3/18/15.
+//  Copyright (c) 2015 Raizlabs. All rights reserved.
+//
+
+#include <dlfcn.h>
+
+#include "RZIOKit.h"
+
+mach_port_t kRZIOMasterPortDefault;
+
+boolean_t (*_RZIOObjectIsEqualTo)(rz_io_object_t object, rz_io_object_t anObject);
+
+CFDictionaryRef (*_RZIOServiceMatching)(const char *name);
+CFDictionaryRef (*_RZIOServiceNameMatching)(const char *name);
+rz_io_service_t (*_RZIOServiceGetMatchingService)(mach_port_t masterPort, CFDictionaryRef matching);
+RZIOReturn (*_RZIOServiceGetMatchingServices)(mach_port_t masterPort, CFDictionaryRef matching, rz_io_iterator_t *iteratorPtr);
+
+boolean_t (*_RZIOIteratorIsValid)(rz_io_iterator_t iterator);
+rz_io_object_t (*_RZIOIteratorNext)(rz_io_iterator_t iterator);
+
+RZIOReturn (*_RZIOServiceOpen)(rz_io_service_t service, task_port_t owningTask, uint32_t type, rz_io_connect_t *connect);
+RZIOReturn (*_RZIOServiceClose)(rz_io_connect_t connect);
+
+RZIOReturn (*_RZIORegistryEntryGetName)(rz_io_registry_entry_t entry, char *name);
+
+
+CFTypeRef (*_RZIORegistryEntryCreateCFProperty)(rz_io_registry_entry_t entry, CFStringRef key, CFAllocatorRef allocator, __unused uint32_t options);
+
+RZIOReturn (*_RZIOObjectRelease)(rz_io_service_t object);
+
+RZIOReturn (*_RZIOPMCopyCPUPowerStatus)(CFDictionaryRef *cpuPowerStatus);
+RZIOReturn (*_RZIOPMGetThermalWarningLevel)(uint32_t *thermalLevel);
+
+const char *kRZIOKitFrameworkPath = "/System/Library/PrivateFrameworks/IOKit.framework/IOKit";
+
+void ut_prepareIOKitSymbols() __attribute__((constructor));
+void ut_prepareIOKitSymbols()
+{
+    void *iokit_handle = dlopen(kRZIOKitFrameworkPath, RTLD_LAZY);
+
+    void *masterPort = dlsym(iokit_handle, "kIOMasterPortDefault");
+    kRZIOMasterPortDefault = masterPort != NULL ? *(mach_port_t *)(masterPort) : 0;
+    _RZIOObjectIsEqualTo = dlsym(iokit_handle, "IOObjectIsEqualTo");
+    _RZIOServiceMatching = dlsym(iokit_handle, "IOServiceMatching");
+    _RZIOServiceNameMatching = dlsym(iokit_handle, "IOServiceNameMatching");
+    _RZIOServiceGetMatchingService = dlsym(iokit_handle, "IOServiceGetMatchingService");
+    _RZIOServiceGetMatchingServices = dlsym(iokit_handle, "IOServiceGetMatchingServices");
+    _RZIOIteratorIsValid = dlsym(iokit_handle, "IOIteratorIsValid");
+    _RZIOIteratorNext = dlsym(iokit_handle, "IOIteratorNext");
+    _RZIOServiceOpen = dlsym(iokit_handle, "IOServiceOpen");
+    _RZIOServiceClose = dlsym(iokit_handle, "IOServiceClose");
+    _RZIORegistryEntryGetName = dlsym(iokit_handle, "IORegistryEntryGetName");
+    _RZIORegistryEntryCreateCFProperty = dlsym(iokit_handle, "IORegistryEntryCreateCFProperty");
+    _RZIOObjectRelease = dlsym(iokit_handle, "IOObjectRelease");
+    _RZIOPMCopyCPUPowerStatus = dlsym(iokit_handle, "IOPMCopyCPUPowerStatus");
+    _RZIOPMGetThermalWarningLevel = dlsym(iokit_handle, "IOPMGetThermalWarningLevel");
+}
+
+#pragma mark - public methods
+
+boolean_t RZIOObjectIsEqualTo(rz_io_object_t object, rz_io_object_t anObject)
+{
+    return _RZIOObjectIsEqualTo(object, anObject);
+}
+
+CFDictionaryRef RZIOServiceMatching(const char *name)
+{
+    return _RZIOServiceMatching(name);
+}
+
+CFDictionaryRef RZIOServiceNameMatching(const char *name)
+{
+    return _RZIOServiceNameMatching(name);
+}
+
+rz_io_service_t RZIOServiceGetMatchingService(mach_port_t masterPort, CFDictionaryRef matching)
+{
+    return _RZIOServiceGetMatchingService(masterPort, matching);
+}
+
+RZIOReturn RZIOServiceGetMatchingServices(mach_port_t masterPort, CFDictionaryRef matching, rz_io_iterator_t *iteratorPtr)
+{
+    return _RZIOServiceGetMatchingServices(masterPort, matching, iteratorPtr);
+}
+
+boolean_t RZIOIteratorIsValid(rz_io_iterator_t iterator)
+{
+    return _RZIOIteratorIsValid(iterator);
+}
+
+rz_io_object_t RZIOIteratorNext(rz_io_iterator_t iterator)
+{
+    return _RZIOIteratorNext(iterator);
+}
+
+RZIOReturn RZIOServiceOpen(rz_io_service_t service, task_port_t owningTask, uint32_t type, rz_io_connect_t *connect)
+{
+    return _RZIOServiceOpen(service, owningTask, type, connect);
+}
+
+RZIOReturn RZIOServiceClose(rz_io_connect_t connect)
+{
+    return _RZIOServiceClose(connect);
+}
+
+RZIOReturn RZIORegistryEntryGetName (rz_io_registry_entry_t entry, char *name)
+{
+    return _RZIORegistryEntryGetName(entry, name);
+}
+
+
+CFTypeRef RZIORegistryEntryCreateCFProperty ( rz_io_registry_entry_t entry, CFStringRef key, CFAllocatorRef allocator, __unused uint32_t options)
+{
+    return _RZIORegistryEntryCreateCFProperty(entry, key, allocator, options);
+}
+
+RZIOReturn RZIOObjectRelease(rz_io_service_t object)
+{
+    return _RZIOObjectRelease(object);
+}
+
+RZIOReturn RZIOPMCopyCPUPowerStatus(CFDictionaryRef *cpuPowerStatus)
+{
+    return _RZIOPMCopyCPUPowerStatus(cpuPowerStatus);
+}
+
+RZIOReturn RZIOPMGetThermalWarningLevel(uint32_t *thermalLevel)
+{
+    return _RZIOPMGetThermalWarningLevel(thermalLevel);
+}
+
+
---
+++ SWABLE/Parasite/SystemInfoTextView.swift
@@ -0,0 +1,136 @@
+//
+//  SystemInfoTextView.swift
+//  Parasite
+//
+//  Created by Jonathan Cole on 1/17/18.
+//  Copyright © 2018 Raizlabs. All rights reserved.
+//
+
+import UIKit
+
+/*
+ Special UITextView that populates itself with info from SystemKit.
+ */
+class SystemInfoTextView: UITextView {
+
+    lazy private var system = System()
+    
+    lazy private var battery: Battery? = {
+        var bt = Battery()
+        if bt.open() != kIOReturnSuccess {
+            return nil
+        }
+        return bt
+    }()
+    
+    public func update() {
+        var textChunks: [String] = []
+        
+        // Add CPU usage info
+        let cpuUsage = system.usageCPU()
+        let counts = System.processCounts()
+        
+        textChunks.append("""
+            ┏━━━━━━━━━━━━━━━━━━━┓
+            CPU
+            ┗━━━━━━━━━━━━━━━━━━━┛
+            
+            Thermal State: \(getThermalStateString())
+            
+            Logical Cores: \(System.logicalCores())
+            System: \(Int(cpuUsage.system))%
+            User: \(Int(cpuUsage.user))%
+            Idle: \(Int(cpuUsage.idle))%
+            
+            Processes: \(counts.processCount)
+            Threads: \(counts.threadCount)
+            """)
+        
+        // Add memory usage info
+        let memoryUsage = System.memoryUsage()
+        
+        //memoryUsageLabel.text = memoryUsageBytes().map { "\($0 / 1024 / 1024) MB" } ?? "Unknown"
+        
+        textChunks.append("""
+            ┏━━━━━━━━━━━━━━━━━━━┓
+            Memory
+            ┗━━━━━━━━━━━━━━━━━━━┛
+            
+            RAM Total: \(memoryUnit(System.physicalMemory()))
+            RAM Free: \(memoryUnit(memoryUsage.free))
+            RAM Used: \(memoryUnit(memoryUsage.active))
+            """)
+        
+        // Add battery info
+        if let battery = battery {
+            textChunks.append("""
+            ┏━━━━━━━━━━━━━━━━━━━┓
+            Battery
+            ┗━━━━━━━━━━━━━━━━━━━┛
+            Charge: \(battery.charge())%
+            \(battery.isACPowered() ? "Plugged in" : "Not plugged in") \(battery.isCharging() ? "(and charging)" : "")
+            """)
+        } else {
+            textChunks.append("""
+            ┏━━━━━━━━━━━━━━━━━━━┓
+            Battery
+            ┗━━━━━━━━━━━━━━━━━━━┛
+
+            Not available!
+            """)
+        }
+        
+        // Write info to text field
+        self.text = textChunks.joined(separator: "\n\n")
+    }
+
+}
+
+// Status info methods (non-StatusKit)
+extension SystemInfoTextView {
+    
+    func getThermalStateString() -> String {
+        switch ProcessInfo.processInfo.thermalState {
+        case ProcessInfo.ThermalState.critical:
+            return "Critical"
+        case ProcessInfo.ThermalState.serious:
+            return "Serious"
+        case .nominal:
+            return "Nominal"
+        case .fair:
+            return "Fair"
+        }
+    }
+    
+    /*
+     Adapted from https://stackoverflow.com/a/39048651
+     Reports the amount of RAM used in bytes.
+     */
+    func memoryUsageBytes() -> UInt64? {
+        var info = mach_task_basic_info()
+        var count = mach_msg_type_number_t(MemoryLayout<mach_task_basic_info>.size)/4
+        
+        let kerr: kern_return_t = withUnsafeMutablePointer(to: &info) {
+            $0.withMemoryRebound(to: integer_t.self, capacity: 1) {
+                task_info(mach_task_self_,
+                          task_flavor_t(MACH_TASK_BASIC_INFO),
+                          $0,
+                          &count)
+            }
+        }
+        
+        if kerr == KERN_SUCCESS {
+            return info.resident_size
+        }
+        else {
+            return nil
+        }
+    }
+    
+    /// Converts a given value in GB to a more reader-friendly format.
+    func memoryUnit(_ value: Double) -> String {
+        if value < 1.0 { return String(Int(value * 1000.0)) + "MB" }
+        else           { return NSString(format:"%.2f", value) as String + "GB" }
+    }
+}
+
