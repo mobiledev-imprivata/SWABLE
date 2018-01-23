39bea43fac7282e4bede16a0e276213c227561d4
Rob Visentin <jvisenti@gmail.com>
2018-01-23 15:01:31 -0500

    fixed pods and aws upload

--- SWABLE/Shared/S3FileManager.swift
+++ SWABLE/Shared/S3FileManager.swift
@@ -25,28 +25,27 @@ class S3FileManager {
     private let awsSecretAccessKey: String = "3acv8TI/nPT8IKEJ6TtEhHAWV3xfnH2LAS8KWMZp"
     private let region = "us-east-1"
     private let service = "s3"
-    private var securityPolicy: String? = Array("""
-    """.utf8).toBase64()
-    
-    init() {
-        
-    }
     
     func upload(text: String) {
         let signDate = Date()
         let data = "Wow".data(using: .utf8)!
-        let timestamp = S3FileManager.amazonDateFormatter.string(from: signDate)
-        let contentSha: String = data.sha256().toHexString()
         
-        let headers: HTTPHeaders = [
+        var headers: HTTPHeaders = [
             "Host": bucketURL,
-            "Content-Type": "text/plain",
-            "x-amz-content-sha256": contentSha,
-            "x-amz-date": timestamp,
-            "Authorization": try! makeAuthorizationHeader(date: signDate, data: data)
         ]
-        
-        Alamofire.upload(data, to: "http://\(bucketURL)/text.txt", method: .put, headers: headers).responseString { (response) in
+
+        headers.amzTimestamp = S3FileManager.amazonDateFormatter.string(from: signDate)
+        headers.amzContentSha = data.sha256().toHexString()
+
+        if let additionalHeaders = SessionManager.default.session.configuration.httpAdditionalHeaders {
+            for header in additionalHeaders {
+                headers[String(describing: header.key)] = String(describing: header.value)
+            }
+        }
+
+        headers["Authorization"] = try? authorization(for: headers)
+
+        Alamofire.upload(data, to: "http://\(bucketURL)/test.txt", method: .put, headers: headers).responseString { (response) in
             switch response.result {
             case .success(let value):
                 print(value)
@@ -58,113 +57,96 @@ class S3FileManager {
                 break
             }
         }
-        
-//        Alamofire.upload(data, to: "http://\(bucketURL)/text.txt", method: .put, headers: headers).responseString { (response) in
-//            if let error = response.error, response.response?.statusCode == 400 {
-//                print("Error: \(error.localizedDescription)")
-//                print("Header: \(headers)")
-//                print("Response: \(response)")
-//            }
-//            print("Response: \(response)")
-//        }
-    }
-    
-    func download() {
-        
     }
     
     /// Value for the Authorization header in any requests this class makes to an S3 bucket.
-    func makeAuthorizationHeader(date: Date, data: Data) throws -> String {
-        
-        guard let signature = try? createSignature(from: date, data: data) else {
+    func authorization(for headers: HTTPHeaders) throws -> String {
+        guard let signature = try? sign(headers) else {
             throw S3Error.RuntimeError("Could not create S3 signature!")
         }
-        
-        let yyyymmdd: String = S3FileManager.yyyymmddFormatter.string(from: date)
-        
-        return """
-        AWS4-HMAC-SHA256 Credential=\(self.awsAccessKeyId)/\(yyyymmdd)/\(self.region)/\(self.service)/aws4_request,SignedHeaders=content-type;host;x-amz-content-sha256;x-amz-date,Signature=\(signature)
-        """
-        // AWS4-HMAC-SHA256 Credential=AKIAIOSFODNN7EXAMPLE/20130524/us-east-1/s3/aws4_request,SignedHeaders=host;range;x-amz-content-sha256;x-amz-date,Signature=f0e8bdb87c964420e857bd35b5d6ed310bd44f0170aba48dd91039c6036bdb41
-        
-    }
-    
-    func makeStringToSign(from date: Date, data: Data) -> String {
-        let timestamp = S3FileManager.amazonDateFormatter.string(from: date)
-        let yyyymmdd: String = S3FileManager.yyyymmddFormatter.string(from: date)
-        let contentSha: String = data.sha256().toHexString()
-        let canonicalRequest = """
-        PUT
-        /test.txt
-        
-        content-type:text/plain
-        host:\(bucketURL)
-        x-amz-content-sha256:\(contentSha)
-        x-amz-date:\(timestamp)
-        
-        content-type;host;x-amz-content-sha256;x-amz-date
-        \(contentSha)
-        """
-        
-        return """
-        AWS4-HMAC-SHA256
-        \(timestamp)
-        \(yyyymmdd)/\(self.region)/\(self.service)/aws4_request
-        \(canonicalRequest.data(using: .utf8)!.sha256().toHexString())
-        """
+
+        let headerKeys = headers.amzSorted.map { $0.key.lowercased() }.joined(separator: ";")
+
+        return "AWS4-HMAC-SHA256 Credential=\(awsAccessKeyId)/\(headers.amzYMD)/\(region)/\(service)/aws4_request,SignedHeaders=\(headerKeys),Signature=\(signature)"
     }
     
     /*
      Creates the string for the Signature field of the Authorization header required for privileged access to S3 buckets.
      See https://docs.aws.amazon.com/AmazonS3/latest/API/sig-v4-authenticating-requests.html
      */
-    func createSignature(from date: Date, data: Data) throws -> String {
-        
-        let stringToSign = makeStringToSign(from: date, data: data)
-        
-        let formattedDate = S3FileManager.yyyymmddFormatter.string(from: date)
-        
-        let dateKey = try HMAC(key: "AWS4\(awsSecretAccessKey)", variant: .sha256).authenticate(formattedDate.utf8Array)
-        let dateRegionKey = try HMAC(key: dateKey, variant: .sha256).authenticate(self.region.utf8Array)
-        let dateRegionServiceKey = try HMAC(key: dateRegionKey, variant: .sha256).authenticate(self.service.utf8Array)
-        let signingKey = try HMAC(key: dateRegionServiceKey, variant: .sha256).authenticate("aws4_request".utf8Array)
-        
-        let signature = try HMAC(key: signingKey, variant: .sha256).authenticate(Array(stringToSign.utf8))
-        return signature.toHexString()
+    func sign(_ headers: HTTPHeaders) throws -> String {
+        var canonicalRequest = """
+        PUT
+        /test.txt
+
+
+        """
+
+        for header in headers.amzSorted {
+            canonicalRequest += "\(header.key.lowercased()):\(header.value)\n"
+        }
+
+        canonicalRequest += """
+
+        \(headers.amzSorted.map { $0.key.lowercased() }.joined(separator: ";"))
+        \(headers.amzContentSha)
+        """
+
+        let stringToSign = """
+        AWS4-HMAC-SHA256
+        \(headers.amzTimestamp)
+        \(headers.amzYMD)/\(region)/\(service)/aws4_request
+        \(canonicalRequest.data(using: .utf8)!.sha256().toHexString())
+        """
+
+        let dateKey = try HMAC(key: "AWS4\(awsSecretAccessKey)", variant: .sha256).authenticate(headers.amzYMD.bytes)
+        let dateRegionKey = try HMAC(key: dateKey, variant: .sha256).authenticate(region.bytes)
+        let dateRegionServiceKey = try HMAC(key: dateRegionKey, variant: .sha256).authenticate(service.bytes)
+        let signingKey = try HMAC(key: dateRegionServiceKey, variant: .sha256).authenticate("aws4_request".bytes)
+
+        return try HMAC(key: signingKey, variant: .sha256).authenticate(stringToSign.bytes).toHexString()
         
     }
-    
-    // Wed, 28 May 2014 19:31:11 +0000
-    private static var headerDateFormatter: DateFormatter = {
-        let formatter = DateFormatter()
-        formatter.dateFormat = "E, d MMM yyyy HH:mm:ss Z"
-        return formatter
-    }()
-    
-    private static var yyyymmddFormatter: DateFormatter = {
-        let formatter = DateFormatter()
-        formatter.dateFormat = "yyyyMMdd"
-        return formatter
-    }()
     
     /// Formats dates as ISO 8601 strings, for use with Amazon S3.
     private static var amazonDateFormatter: ISO8601DateFormatter = {
         let formatter = ISO8601DateFormatter()
         formatter.formatOptions.remove(.withColonSeparatorInTime)
         formatter.formatOptions.remove(.withDashSeparatorInDate)
-//        formatter.calendar = Calendar(identifier: .iso8601)
-//        formatter.locale = Locale(identifier: "en_US_POSIX")
-//        formatter.timeZone = TimeZone(secondsFromGMT: 0)
-//        formatter.dateFormat = "yyyyMMdd'T'HHmmss'Z'"
         return formatter
     }()
-    
-    
-    
+
 }
 
-extension String {
-    var utf8Array: Array<UInt8> {
-        return Array(self.utf8)
+private extension Dictionary where Key == String, Value == String {
+
+    var amzSorted: [(key: String, value: String)] {
+        return sorted(by: { a, b -> Bool in
+            a.key < b.key
+        })
     }
+
+    var amzTimestamp: String {
+        get {
+            return self["x-amz-date"] ?? ""
+        }
+        set {
+            self["x-amz-date"] = newValue
+        }
+    }
+
+    var amzYMD: String {
+        let timestamp = amzTimestamp
+        return String(timestamp.index(of: "T").map { timestamp[..<$0] } ?? "")
+    }
+
+    var amzContentSha: String {
+        get {
+            return self["x-amz-content-sha256"] ?? ""
+        }
+        set {
+            self["x-amz-content-sha256"] = newValue
+        }
+    }
+
 }
